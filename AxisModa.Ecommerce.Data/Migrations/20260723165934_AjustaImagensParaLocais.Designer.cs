5be9a01aee3738495e98db73fe16b5418c60c02a
RICHARD CORREA <desenvolvedor.rc@gmail.com>
2026-07-23 14:03:15 -0300

    feat: altera origem dos produtos para caminhos de imagem locais

--- AxisModa.Ecommerce.Data/Migrations/20260723165934_AjustaImagensParaLocais.Designer.cs
+++ AxisModa.Ecommerce.Data/Migrations/20260723165934_AjustaImagensParaLocais.Designer.cs
@@ -4,6 +4,7 @@ using AxisModa.Ecommerce.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AxisModa.Ecommerce.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260723165934_AjustaImagensParaLocais")]
+    partial class AjustaImagensParaLocais
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AxisModa.Ecommerce.Data/Migrations/20260723165934_AjustaImagensParaLocais.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AxisModa.Ecommerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustaImagensParaLocais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("60762f4d-bc3f-4221-b306-e5f7ba2920e9"),
+                column: "ImagemUrl",
+                value: "/uploads/produtos/vestido.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("9f8c7bcf-6f68-459b-a7b8-a733370f2cb7"),
+                column: "ImagemUrl",
+                value: "/uploads/produtos/camisa.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("aeaaec9f-a407-4909-b6c9-a65f7356a263"),
+                column: "ImagemUrl",
+                value: "/uploads/produtos/infantil.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("bb98a8d4-4e0e-46db-bef6-56cc77ecf145"),
+                column: "ImagemUrl",
+                value: "/uploads/produtos/blusa.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("60762f4d-bc3f-4221-b306-e5f7ba2920e9"),
+                column: "ImagemUrl",
+                value: "https://unsplash.com");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("9f8c7bcf-6f68-459b-a7b8-a733370f2cb7"),
+                column: "ImagemUrl",
+                value: "https://unsplash.com");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("aeaaec9f-a407-4909-b6c9-a65f7356a263"),
+                column: "ImagemUrl",
+                value: "https://unsplash.com");
+
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "Id",
+                keyValue: new Guid("bb98a8d4-4e0e-46db-bef6-56cc77ecf145"),
+                column: "ImagemUrl",
+                value: "https://unsplash.com");
+        }
+    }
+}
